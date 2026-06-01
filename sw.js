dc39b864d17ebd0387808265483c6fdec760e4e0
smuhs23 <chris.lively88@googlemail.com>
2026-06-01 15:06:38 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE = 'ladepark-v1';
-const ASSETS = ['./', './index.html', './styles.css', './src/ui.js', './src/engine.js', './src/store.js', './src/charts.js', './src/export.js'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
